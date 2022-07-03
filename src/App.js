f66946e97f2669dd3d3ebde83a254410d905d539
Samuel Sampeur <sampeur4life1@gmail.com>
2022-07-03 04:44:37 -0700

    Configure routes of pages and install importantes packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/homes/Home';
+import Sampeurs from './pages/sampeurs/Sampeurs';
+import NotFound from './pages/notFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Suspense fallback="loading"><Home /></Suspense>}/>
+        <Route exact path="/sampeurs" element={<Suspense fallback="loading"><Sampeurs /></Suspense>} />
+        <Route path="*" element={<Suspense fallback="loading"><NotFound /></Suspense>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
