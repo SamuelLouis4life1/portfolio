b33591b7baf2531b0a911e63ff0b67b2dccf3ccd
Samuel Sampeur <sampeur4life1@gmail.com>
2022-07-03 05:10:02 -0700

    Css files an Social midias component

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/socialMidia.css";
+
+
+export function SocialMidia() {
+    return (
+        <div className="social_buttons">
+            <a href=""><i className="fa fa-instagram"></i></a>
+            <a href=""><i className="fa fa-facebook"></i></a>
+            <a href=""><i className="fa fa-behance"></i></a>
+            <a href=""><i className="fa fa-dribbble"></i></a>
+            <a href=""><i className="fa fa-twitter"></i></a>
+        </div>
+    )
+}
+export default SocialMidia;
