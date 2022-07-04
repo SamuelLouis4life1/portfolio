0a4ebc0252de548be664ac84cbefae213081636a
Samuel Sampeur <sampeur4life1@gmail.com>
2022-07-03 19:44:33 -0700

    Resolve error translation: Uncaught TypeError: this.props.i18n.changeLanguage is not a function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import i18n from './i18n'
+import { I18nextProvider } from 'react-i18next';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <I18nextProvider i18n={i18n}>
     <App />
-  </React.StrictMode>
+  </I18nextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
