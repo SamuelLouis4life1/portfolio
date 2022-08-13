52ac24a9cf1b741c992be0443e44ecf91776f4a0
Samuel Sampeur <sampeur4life1@gmail.com>
2022-08-12 21:44:01 -0700

    Follow button in with using module

--- src/consts.js
+++ src/consts.js
@@ -1,18 +1,18 @@
-export const Instagram = "https://www.instagram.com";
+export const Instagram = "https://www.instagram.com/";
 export const Facebook = "https://web.facebook.com";
 export const Twitter = "https://twitter.com";
-export const GitHub = "https://github.com";
-export const LinkedIn = "https://www.linkedin.com";
+export const GitHub = "https://github.com/";
+export const LinkedIn = "https://www.linkedin.com/";
 export const Google = "https://mail.google.com/mail/u/0/#inbox?compose=new";
 export const Behance = "https://www.behance.net/gallery/58902999/Login";
 export const Dribbble = "https://dribbble.com/session/new";
 
 export const UrlSocialMedia = {
-    User1Insta: Instagram + "/samuel.louis1991/",
+    myInsta: Instagram + "samuel.louis1991",
     User1Facebook: Facebook + "",
     User1Twitter: Twitter + "/in/louis-samuel-544082110/",
-    User1GitHub: GitHub + "/in/louis-samuel-544082110/",
-    User1LinkedIn: LinkedIn + "/in/louis-samuel-544082110/",
+    myGitHub: GitHub + "SamuelLouis4life1",
+    myLinkedIn: LinkedIn + "in/louis-samuel-544082110/",
     User1Google: Google + "/in/louis-samuel-544082110/",
     User1Behance: Behance + "/in/louis-samuel-544082110/",
     User1Dribbble: Dribbble + "/in/louis-samuel-544082110/",
