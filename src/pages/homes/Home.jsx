f5fe826762ec8653d9cb0f031522d84c004a165b
Samuel Sampeur <sampeur4life1@gmail.com>
2022-08-13 20:01:13 -0700

    change color svg on hover

--- src/pages/homes/Home.jsx
+++ src/pages/homes/Home.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Spinner from "../../components/elements/spinner/index"
 import HomeIndex from "../homes/home/index";
 import About from "./about";
 import Skills from "../../pages/homes/skills/index";
 import Portfolio from "../../pages/homes/portfolio/index"
 import Footer from "../../components/layouts/footer/index"
 
-export default function Home(){
+export default function Home() {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 4000);
+    }, []);
+
     return (
         <>
-        <HomeIndex />
-        <About />
-        <Skills />
-        {/* <Portfolio /> */}
-        {/* <Footer /> */}
+            {loading ? (
+                <Spinner />
+            ) : (
+                <>
+                    <HomeIndex />
+                    <About />
+                    <Skills />
+                    <Portfolio />
+                    {/* <Footer /> */}
+                </>
+            )}
         </>
     );
 }
