8946da3d8fab470f87462730f7ff8a6335e8ddd5
Samuel Sampeur <sampeur4life1@gmail.com>
2022-08-20 08:29:18 -0700

    Standarize buttons in about me section

--- src/pages/homes/about/index.jsx
+++ src/pages/homes/about/index.jsx
@@ -4,62 +4,22 @@ import styles from "./index.module.css";
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap"
 import { Container } from "react-bootstrap";
-import { withTranslation } from "react-i18next";
 import AboutImg from "../../../images/logo.png";
 import { HashLink } from 'react-router-hash-link';
-import i18n from "i18next";
-// import portuges from "/cv_portugues.pdf"
+
+import { useTranslation } from "react-i18next";
 // import SmoothList from "react-smooth-list";
-// import $ from 'jquery';
 // import 'animate.css';
 import portugues from '../../../pdf/cv_portugues.pdf';
 import english from '../../../pdf/cv_english.pdf';
 import francais from '../../../pdf/cv_creol.pdf';
 import espanol from '../../../pdf/cv_creol.pdf';
 import criolo from '../../../pdf/cv_creol.pdf';
-import { App } from "../../../components/elements/textCarousel/index"
+import { FaDownload } from "react-icons/fa";
 
-
-// import MyPDFs from '/cv_creol.pdf';
-
-export function About(props) {
-  const { t } = props;
-
-  const [download, setDownload] = useState('');
-  const [userOs, setUserOS] = useState();
-  const LinksToDownloadCv = {
-    Portugues: portugues,
-    English: english,
-    Francais: francais,
-    Espanol: espanol,
-    Criolo: criolo
-  }
-
-  useEffect(() => {
-    setUserOS(() => detectOS());
-  }, []);
-
-  function detectOS(lang) {
-
-    if (i18n.language === "ptbr") {
-      return "Portugues";
-    }
-    if (i18n.language === "en") {
-      return "English";
-    } else if (i18n.language === "fr") {
-      return "Francais";
-    } else if (i18n.language === "es") {
-      return "Espanol"
-    } else if (i18n.language === "ht") {
-      return "Criolo"
-    } else {
-      return null;
-    }
-  }
-  
-  useEffect(() => {
-    setDownload(LinksToDownloadCv[userOs]);
-  }, [userOs]);
+export default function About(props) {
+  const { ts, i18n } = useTranslation();
+  const { t } = i18n;
 
   // (function () {
   //   var words = ["Fullstack Developer", "Developer"],
@@ -71,29 +31,8 @@ export function About(props) {
   //   }, 5000)
   // })();
 
-  // constructor(props) {
-  //   super(props);
-  //   console.log("--->" + props.language);
-  //   this.state = {
-  //     value: props.language ? props.language : props.i18n.language,
-  //   };
-  // }
-
-  // onLanguageHandle = (lang) => {
-  //   let newLang = lang;
-  //   console.log(lang);
-
-  //   this.setState({ value: newLang });
-  //   this.props.i18n.changeLanguage(newLang);
-  // }
-
   return (
     <>
-      {console.log("langDown", i18n.language)}
-      {console.log("langDown2", props.language)}
-      {console.log("langDown1", download)}
-
-
       {/* <SmoothList visible="true"> */}
       {/* <Container fluid padding="0%"> */}
       <section id="about" className={styles.aboutSection}>
@@ -105,7 +44,7 @@ export function About(props) {
             {/* <div class={styles.aboutImg}></div> */}
             <img src={AboutImg} alt="" />
             <p id="words" className={styles.aboutImgTitle}>{t("about.bigText2")}</p>
-            <p className={styles.aboutImgSubTitle}>{t("about.bigText5")}<Link to={"/sampeurs"} className="link">{t("sampeurs")}</Link></p>
+            <p className={styles.aboutImgSubTitle}>{t("about.bigText5")}<Link to={"/sampeurs"} className="link">{("sampeurs")}</Link></p>
           </div>
 
           <div className={styles.aboutRightContents}>
@@ -123,38 +62,9 @@ export function About(props) {
 
             <p>{t("about.descriptionP6")} {t("about.moreAboutme")} <button>{t("about.showMore")}</button></p>
 
-
-            <div className="about-item about-btn">
-              <button class="blob-btn">
-                {t("about.aboutMeBtn")}
-                <span className="blob-btn__inner">
-                  <span className="blob-btn__blobs">
-                    <span className="blob-btn__blob"></span>
-                    <span className="blob-btn__blob"></span>
-                    <span className="blob-btn__blob"></span>
-                    <span className="blob-btn__blob"></span>
-                  </span>
-                </span>
-              </button>
-
-              <button className="blob-btn" value={userOs} onChange={(e) => setUserOS(e.target.value)}>
-                {t("about.aboutMeCv")}
-                <span className="blob-btn__inner">
-                  <span className="blob-btn__blobs">
-                    <span className="blob-btn__blob"></span>
-                    <span className="blob-btn__blob"></span>
-                    <span className="blob-btn__blob"></span>
-                    <span className="blob-btn__blob"></span>
-                  </span>
-                </span>
-              </button>
-              <a download={download}><button>Download</button></a>
-              {/* <a href="../../../images/cv_creol.pdf" target="_blank" rel="noopener noreferrer" download>Download test</a> */}
-              <a href="/images/cv_creol.pdf" target="_blank" rel="noopener noreferrer" download>Download test</a>
-              <a href="../../../images/cv_creol.pdf" download>Download the pdf</a>
-              <a id="downloadLink" href="../../../images/cv_creol.pdf" target="_blank" type="application/octet-stream" download>click here</a>
-              {/* <a href={download} download="My_File.pdf"> Download Here teste </a> */}
-              <a href={download} download="My_File.pdf"> Download Here teste </a>
+            <div className={styles.aboutBtns}>
+              <CVButton />
+              <ContactMe />
             </div>
           </div>
         </div>
@@ -168,4 +78,65 @@ export function About(props) {
   );
 }
 
-export default withTranslation()(About);
+function CVButton(props) {
+  const { ts, i18n } = useTranslation();
+  const { t } = i18n;
+
+  const [download, setDownload] = useState('');
+  const [userOs, setUserOS] = useState();
+  const LinksToDownloadCv = {
+    Portugues: portugues,
+    English: english,
+    Francais: francais,
+    Espanol: espanol,
+    Criolo: criolo
+  }
+
+  useEffect(() => {
+    setUserOS(() => detectOS());
+  }, []);
+
+  function detectOS(lang) {
+    if (i18n.language === "ptbr") {
+      return "Portugues";
+    }
+    if (i18n.language === "en") {
+      return "English";
+    } else if (i18n.language === "fr") {
+      return "Francais";
+    } else if (i18n.language === "es") {
+      return "Espanol"
+    } else if (i18n.language === "ht") {
+      return "Criolo"
+    } else {
+      return null;
+    }
+  }
+
+  useEffect(() => {
+    setDownload(LinksToDownloadCv[userOs]);
+  }, [userOs]);
+
+  return (
+    <>
+      {console.log("langDown", i18n.language)}
+      {console.log("langDown2", props.language)}
+      {console.log("langDown1", download)}
+
+      <div class={styles.hover}><span>{t("about.aboutMeCv")}</span>
+        <a href={download} download="Samuel Louis's CV.pdf"> <FaDownload />{t("about.aboutBtnCv")}</a>
+      </div>
+    </>
+  )
+}
+
+function ContactMe() {
+  const { ts, i18n } = useTranslation();
+  const { t } = i18n;
+
+  return (
+    <div class={styles.hover}><span>{t("about.contactBtn")}</span>
+      <a> <HashLink to="/#contact" className="link"> {t("about.contactBtn")} </HashLink></a>
+    </div>
+  )
+}
