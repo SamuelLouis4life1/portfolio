0da573dc05106dc5ea8f7a428bf7a007f2f2d867
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2023-10-21 21:39:47 -0700

    Remove services section

--- src/pages/homes/Home.jsx
+++ src/pages/homes/Home.jsx
@@ -31,7 +31,7 @@ export default function Home() {
                     <About />
                     <Skills />
                     <Experiences />
-                    <Services />
+                    {/* <Services /> */}
                     <Portfolio />
                     <Contact />
                     <Footer />
